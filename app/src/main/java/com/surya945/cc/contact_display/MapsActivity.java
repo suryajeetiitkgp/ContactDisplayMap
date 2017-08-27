6c5978e82289e3aa991f970571cfbb261dce9108
Surya Jeet Singh <suryajeetiitkgp@gmail.com>
2017-08-28 00:56:45 +0530

    Added Gyro

--- app/src/main/java/com/surya945/cc/contact_display/MapsActivity.java
+++ app/src/main/java/com/surya945/cc/contact_display/MapsActivity.java
@@ -10,6 +10,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
